1a0dc2aa1e15d146738ab3c2437c0183133f3d23
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-06-16 14:16:01 +0100

    Add argument to update current date and time in models

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    # The auto_now argument for the updated_on field sets the value 
+    # to the current date and time whenever the record is saved, 
+    # not just when it is created.
